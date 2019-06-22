f80a3565f5bcc303f43a634f0cd78bce552328de
Gleb <zadachings@gmail.com>
2019-06-22 15:14:46 +0300

    Moved some assets into not LFS folder

--- Source/TestingGrounds/TestingGroundsGameMode.cpp
+++ Source/TestingGrounds/TestingGroundsGameMode.cpp
@@ -9,7 +9,7 @@ ATestingGroundsGameMode::ATestingGroundsGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behaviour/Blueprints/FirstPersonCharacter"));  //FirstPersonCPP/Blueprints/FirstPersonCharacter
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Player/Behaviour/Blueprints/FirstPersonCharacter"));  //FirstPersonCPP/Blueprints/FirstPersonCharacter
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
